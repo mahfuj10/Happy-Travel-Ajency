ee433245c2b85cd3560174826d88ae9652a1e62c
mahfuj10 <mahfujglobal@gmail.com>
2021-08-01 13:24:35 +0600

    add new section

--- counter.js
+++ counter.js
@@ -1,11 +1,16 @@
+// counting
 $(document).ready(function(){
     $(".counter").counterUp({
         delay: 10,
         time: 1200
     });
 });
-
+// data aos
 AOS.init({
     duration: 2000,
     once: true,
     });
+// preloader
+setTimeout(function(){
+        $('.loader-bg').fadeToggle();
+    },2000);
